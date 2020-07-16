55c2ef4a0324adfc4962141b189b112fb7650c79
Dani Bassett <dani@danithetallgirl.com>
2020-07-16 00:40:50 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,24 @@
+class Song
+  attr_accessor :name, :artist, :genre
+  
+  @@count = 0
+  @@artists = 0
+  @@genres = 0
+  
+  def initialize(name, artists, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+  end
+  
+  def self.count
+    @@count
+  end
+  
+  def self.artists
+    @@artists
+  end
+  
+  def self.genres
+    @@genres
+end
